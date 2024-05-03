a751e63d15a93b787b76d34015ff5160f82c1db7
Софья <113128986+Painelka@users.noreply.github.com>
2024-05-03 22:42:22 +0200

    Update save.php

--- Lab4/code/save.php
+++ Lab4/code/save.php
@@ -9,20 +9,20 @@ function redirectToHome(): void
 if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])){
     redirectToHome();
 }
-
+$email =$_POST['email'];
 $category =$_POST['category'];
 $title =$_POST['title'];
 $description =$_POST['description'];
 
 
-require  'C:\Users\Софья\Desktop\lab4\Lab4\code\vendor\autoload.php';
+require  'vendor/autoload.php';
 $client = new \Google_Client();
 $client->setApplicationName('Google sheets and php');
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 $client->setAccessType('ofline');
 try
 {
-    $client->setAuthConfig('C:\Users\Софья\Desktop\lab4\Lab4\code\credentials.json');
+    $client->setAuthConfig('credentials.json');
 }
 catch (\Google\Exception $e)
 {
@@ -32,11 +32,18 @@ $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1SIro9lyvc5gQJIdyUHrJaE0KTCeMxENqUiNNsQgq0QQ";
 
 $range = "List1";
-$values =[[$category, $name, $email, $desc],];
+$values =[[$category, $email, $title, $$description],];
 $body = new Google_Service_Sheets_ValueRange(['values' => $values]);
-$row = sizeof(($service->spreadsheets_values->get($sheetID, $range))->getValues()) + 1;
+$row = sizeof(($service->spreadsheets_values->get($spreadsheetId , $range))->getValues()) + 1;
 $params = ['valueInputOption'=>'RAW'];
-
+try
+{
+    $service->spreadsheets_values->update($spreadsheetId, 'List1!A'.($row), $body, $params);
+}
+catch (\Google\Service\Exception $e)
+{
+    echo "Ошибка";
+}
 
 redirectToHome();
 
