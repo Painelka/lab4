e436559af8699f88d0ba65cf71b5185fcf03a190
Софья <113128986+Painelka@users.noreply.github.com>
2024-05-03 19:35:56 +0200

    Update sheets.php

--- Lab4/code/sheets.php
+++ Lab4/code/sheets.php
@@ -1,6 +1,6 @@
 <?php
 
-require _DIR_.'/vendor/autoload.php';
+require _DIR_ . '/Lab4/code/vendor/autoload.php';
 
 $client = new \Google_Client();
 $client->setApplicationName('Google sheets and php');
@@ -9,3 +9,17 @@ $client->setAccessType('ofline');
 $client->setAuthConfig(_DIR_.'/credentials.json');
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1SIro9lyvc5gQJIdyUHrJaE0KTCeMxENqUiNNsQgq0QQ";
+
+$range = "List1!A2:C3";
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+if(empty($values)){
+    print "No data found.\n";
+}
+else{
+    $mask="%10s %-10s %s\n";
+    foreach($values as $row){
+        echo sprintf($mask, $row[2], $row[1], $row[0]);
+    }
+
+}
