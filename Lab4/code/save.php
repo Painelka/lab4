cfb7edebb43a786d44669b3fcb8e19a631550d8b
Софья <113128986+Painelka@users.noreply.github.com>
2024-05-03 22:03:24 +0200

    Update save.php

--- Lab4/code/save.php
+++ Lab4/code/save.php
@@ -14,9 +14,37 @@ $category =$_POST['category'];
 $title =$_POST['title'];
 $description =$_POST['description'];
 
+
+require  'C:\Users\Софья\Desktop\lab4\Lab4\code\vendor\autoload.php';
+$client = new \Google_Client();
+$client->setApplicationName('Google sheets and php');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('ofline');
+try
+{
+    $client->setAuthConfig('C:\Users\Софья\Desktop\lab4\Lab4\code\credentials.json');
+}
+catch (\Google\Exception $e)
+{
+    echo "Ошибка\n";
+}
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = "1SIro9lyvc5gQJIdyUHrJaE0KTCeMxENqUiNNsQgq0QQ";
+
+
+
+
+
+
+
+
+
 $filePath ="categories/{$category}/{$title}.txt";
 if (false === file_put_contents($filePath, $description)){
     throw new Exception(message: 'Something went wrong.');
 }
 chmod($filePath, 0777);
 redirectToHome();
+
+
+
