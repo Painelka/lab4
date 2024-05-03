662af3b4f3dc091872d8842602914d83d3a66ef4
Софья <113128986+Painelka@users.noreply.github.com>
2024-05-03 21:49:38 +0200

    Update index.php

--- Lab4/code/index.php
+++ Lab4/code/index.php
@@ -21,14 +21,11 @@
         <label for="email">Email</label>
         <input type ="email" name="email" required>
         <label for ="category">Category </label>
-
         <?php
         $categories = scandir('categories');
         echo '<select name="category" required>';
         foreach ($categories as $category) {
             if ((is_dir("categories/$category")) && ($category != '.') && ($category != '..')) {
-                // Проверяем, является ли текущий элемент папкой И не является текущей или родительской директорией.
-                // Если это так, то этот элемент добавляется в список категорий для отображения.
                 echo "<option value='$category'>$category</option>";
             }
         }
@@ -43,15 +40,44 @@
 
         <input type ="submit" value = "save">
     </form>
-
+    <?php
+    require  'C:\Users\Софья\Desktop\lab4\Lab4\code\vendor\autoload.php';
+    $client = new \Google_Client();
+    $client->setApplicationName('Google sheets and php');
+    $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+    $client->setAccessType('ofline');
+    try
+    {
+        $client->setAuthConfig(__DIR__ . '/web-lab4-422209-76913d7c4a42.json');
+    }
+    catch (\Google\Exception $e)
+    {
+        echo "Ошибка\n";
+    }
+    $client->setAuthConfig('C:\Users\Софья\Desktop\lab4\Lab4\code\credentials.json');
+    $service = new Google_Service_Sheets($client);
+    $spreadsheetId = "1SIro9lyvc5gQJIdyUHrJaE0KTCeMxENqUiNNsQgq0QQ";
+    ?>
+    
 </div>
 
 <div id ="table">
     <table border="1" width="400">
         <thead >
-        <th>Category</th>
-        <th>Title</th>
-        <th>Description</th>
+        $range1 = "List1!A1:C1";
+        $result1 = null;
+        try {
+            $result1 = ($service->spreadsheets_values->get($sheetID, $range1))->getValues();
+        }
+        catch (\Google\Service\Exception $e) {
+            echo "Ошибка при получении заголовков в таблицу\n";
+        }
+        if (null != $result1) {
+            foreach ($result1 as $row) {
+                foreach ($row as $item)
+                    echo "<th>$item</th>";
+                }
+            }
         </thead>
         <tbody>
         <?php
@@ -82,6 +108,7 @@
             }
         }
         ?>
+
         </tbody>
     </table>
 </div>
