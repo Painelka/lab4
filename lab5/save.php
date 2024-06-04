20a7c545a3d5f4875ab68f40fc664e37ada9f67e
Софья <113128986+Painelka@users.noreply.github.com>
2024-06-05 00:31:54 +0200

    Create save.php

---
+++ lab5/save.php
@@ -0,0 +1,17 @@
+<?php
+
+function redirectToHome(): void
+{
+    header( header:'Location: /');
+    exit();
+}
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])){
+    redirectToHome();
+}
+$email =$_POST['email'];
+$title =$_POST['title'];
+$description =$_POST['description'];
+$category =$_POST['category'];
+$db = extracted();
+$command = $db->query("INSERT INTO web.ad (email, title, description, category) VALUES ( '{$email}', '{$title}', '{$description}', '{$category}' )");
+redirectToHome();
